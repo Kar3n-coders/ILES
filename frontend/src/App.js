2993ca06c20481a6a8f4f7fcef24a2d60d687e04
Matthew Mumbere <matthewmumbere@gmail.com>
2026-04-07 01:18:15 +0300

    feat(routing): writing the routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,58 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+
+import { AuthContext } from "./context/AuthContext";
+
+import ProtectedRoute from "./components/routing/ProtectedRoute";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import StudentDashboardPage from "./pages/StudentDashboardPage";
+import SupervisorDashboardPage from "./pages/SupervisorDashboardPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import LogbookPage from "./pages/LogbookPage";
+import EvaluationPage from "./pages/EvaluationPage";
+import NotPageFound from "./pages/NotPageFound";
+
 import "./styles/base.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AuthContext>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              {/* Student Routes*/}
+              <Route
+                path="/student/dashboard"
+                element={<StudentDashboardPage />}
+              />
+              <Route path="/student/logbook" element={<LogbookPage />} />
+
+              {/* Supervisor Routes*/}
+              <Route
+                path="/supervisor/dashboard"
+                element={<SupervisorDashboardPage />}
+              />
+              <Route
+                path="/supervisor/evaluation"
+                element={<EvaluationPage />}
+              />
+
+              {/* Admin Routes*/}
+              <Route path="/admin" element={<AdminDashboardPage />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotPageFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext>
   );
 }
 
