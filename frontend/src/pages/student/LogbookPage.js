d32f0ce137a2e8c30c6de93b0654378d845ad886
ANKUNDA REAGAN <reaganankunda4@gmail.com>
2026-05-02 18:24:48 +0300

    feat-ILES-33-still-working-on-LogBook.js

--- frontend/src/pages/student/LogbookPage.js
+++ frontend/src/pages/student/LogbookPage.js
@@ -30,3 +30,4 @@ const handleSubmit = async (id) => {
   const update = await submitLogbook(id);
   setLogs(prev => prev.map(1 => 1.id == id ? updated : 1));
 };
+const handledelete async (id) => { s}
