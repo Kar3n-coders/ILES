8226c7147bff96041af022ac03aab86e9fab460e
ANKUNDA REAGAN <reaganankunda4@gmail.com>
2026-05-05 01:26:35 +0300

    ILES-39-Importing HomePage.css

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -10,3 +10,8 @@ export default HomePage;
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import {
+  GraduationCap, BookOpen, ClipboardCheck, Star, Building2, Shield,
+  ArrowRight, Moon, Sun, ChevronRight, Clock, Users, CheckCircle2,
+} from "lucide-react";
+import "./HomePage.css";
