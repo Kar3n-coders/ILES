2b13ad3df95209998f8eef639d88302a264ddbfe
Matthew Mumbere <matthewmumbere@gmail.com>
2026-05-05 16:23:36 +0300

    [ILES-35]: creating chart icon

--- frontend/src/components/common/Icons.js
+++ frontend/src/components/common/Icons.js
@@ -27,4 +27,18 @@ export const I = {
       <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
     </svg>
   ),
+  chart: (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="18" y1="20" x2="18" y2="10" />
+      <line x1="12" y1="20" x2="12" y2="4" />
+      <line x1="6" y1="20" x2="6" y2="14" />
+    </svg>
+  ),
 };
