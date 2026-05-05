5140521e785f278ffabb56d33d6eb5fae164f248
Matthew Mumbere <matthewmumbere@gmail.com>
2026-05-05 18:42:12 +0300

    [ILES-35]: creating search icon

--- frontend/src/components/common/Icons.js
+++ frontend/src/components/common/Icons.js
@@ -135,4 +135,17 @@ export const I = {
       <line x1="5" y1="12" x2="19" y2="12" />
     </svg>
   ),
+  search: (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    </svg>
+  ),
 };
