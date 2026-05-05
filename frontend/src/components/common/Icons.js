f99c6269a176a06a223a72fbf30e69ccd305d321
Matthew Mumbere <matthewmumbere@gmail.com>
2026-05-05 18:48:25 +0300

    [ILES-35]: creating arrow icon

--- frontend/src/components/common/Icons.js
+++ frontend/src/components/common/Icons.js
@@ -181,4 +181,17 @@ export const I = {
       <circle cx="18" cy="12" r="1.6" />
     </svg>
   ),
+  arrow: (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="5" y1="12" x2="19" y2="12" />
+      <polyline points="12 5 19 12 12 19" />
+    </svg>
+  ),
 };
