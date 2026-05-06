29da142e92674a2a8b84d977117ce0711065277b
Nakibuuka Karen <k41444105@gmail.com>
2026-05-06 16:00:15 +0300

    [ILES-49]:fixing the progresspage.js file

--- frontend/src/pages/student/ProgressPage.js
+++ frontend/src/pages/student/ProgressPage.js
@@ -1,8 +1,8 @@
 const SKILLS = [
     ["REST APIs", 80],
-    ["Database Design" 65],
+    ["Database Design", 65],
     ["Version control", 90],
     ["Team communication", 55],
     ["Testing", 35],
     ["DevOps /Docker", 48],
-]
+];
