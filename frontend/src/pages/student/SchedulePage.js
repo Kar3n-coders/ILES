60a409393aeab0893efc18799e7223beee901bb0
Sabano Zubedah Angel <sabanozubedah@gmail.com>
2026-06-06 13:10:02 +0300

    feat(ILES-57): starting Student Schedule Page

---
+++ frontend/src/pages/student/SchedulePage.js
@@ -0,0 +1,6 @@
+import { PageHead, Card, Chip } from "../../components/common/Primitives";
+import "./SchedulePage.css";
+
+const SCHEDULE = [
+    {week: "Week 5", date: "2026-05-12 - 2026-05-16", title: "Workplace supervisor visit", }
+]
